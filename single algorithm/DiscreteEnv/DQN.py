6db06e679468263b50c099720cf307e47baed77c
HongyingMa <1729037772@sjtu.edu.cn>
2021-07-12 12:27:15 +0800

    add notes for  DQN.

--- single algorithm/DiscreteEnv/DQN.py
+++ single algorithm/DiscreteEnv/DQN.py
@@ -6,42 +6,34 @@ import torch.nn as nn
 import torch
 from torch.optim import Adam
 from typing import Tuple
+import os
 
 
 class ReplayBuffer:
-    def __init__(self, obs_dim: int, batch_size: int = 128, memory_size: int = 2 * 10,
-                 device=torch.device('cpu')) -> None:
-        '''
-        :param obs_dim: the dim of observation. type: int. for gym env: obs_dim = env.observation_space.shape[0]
-        :param batch_size: each time the class will sample the "batch_size" items from ReplayBuffer.
-        :param memory_size:  total size of ReplayBuffer.
-        :param device: when using sample func, the samples will put into cpu or gpu.
-        '''
-        self._memory_size = memory_size
-        self.obs_dim = obs_dim
-        # column: (s, a, r, done, s_) = 2 * obs_dim + 3 constants.
-        self.buffer = np.zeros((self._memory_size, obs_dim * 2 + 3), dtype=np.float32)
-        self._batch_size = batch_size
-        self._memory_counter = 0
+    def __init__(self, state_dim, max_size=10000, device=torch.device('cpu')):
         self.device = device
+        self.state_buffer = torch.empty((max_size, state_dim), dtype=torch.float32, device=device)
+        self.other_buffer = torch.empty((max_size, 3), dtype=torch.float32, device=device)
+        self.index = 0
+        self.max_size = max_size
+        self.total_len = 0
 
-    def store_transition(self, s: np.ndarray, a: int, r: float, done: float, s_: np.ndarray) -> None:
-        transition = np.hstack((s, a, r, done, s_))
-        index = self._memory_counter % self._memory_size
-        self.buffer[index, :] = transition
-        self._memory_counter += 1
+    def append(self, state, other):
+        self.index = self.index % self.max_size
+        self.total_len = max(self.index, self.total_len)
+        self.state_buffer[self.index] = torch.as_tensor(state, device=self.device)
+        self.other_buffer[self.index] = torch.as_tensor(other, device=self.device)
+        self.index += 1
 
-    def sample_batch(self):
-        index = np.random.choice(min(self._memory_counter, self._memory_size), self._batch_size)
-        batch_state = torch.as_tensor(self.buffer[index, :self.obs_dim], dtype=torch.float32, device=self.device)
-        batch_action = torch.as_tensor(self.buffer[index, self.obs_dim:self.obs_dim + 1], dtype=torch.long,
-                                       device=self.device)
-        batch_reward = torch.as_tensor(self.buffer[index, self.obs_dim + 1:self.obs_dim + 2], dtype=torch.float32,
-                                       device=self.device)
-        batch_mask = torch.as_tensor(self.buffer[index, self.obs_dim + 2:self.obs_dim + 3], dtype=torch.float32,
-                                     device=self.device)
-        batch_state_ = torch.as_tensor(self.buffer[index, -self.obs_dim:], dtype=torch.float32, device=self.device)
-        return batch_state, batch_action, batch_reward, batch_mask, batch_state_
+    def sample_batch(self, batch_size):
+        indices = np.random.randint(0, self.total_len - 1, batch_size)
+        return (
+            self.state_buffer[indices],  # S_t
+            self.other_buffer[indices, 2:].long(),  # a_t
+            self.other_buffer[indices, 0],  # r_t
+            self.other_buffer[indices, 1],  # done
+            self.state_buffer[indices + 1]
+        )
 
 
 class QNet(nn.Module):
@@ -57,7 +49,6 @@ class QNet(nn.Module):
         self.encoder = nn.Sequential(
             nn.Linear(obs_dim, mid_dim), nn.ReLU(),
             nn.Linear(mid_dim, mid_dim), nn.ReLU(),
-            nn.Linear(mid_dim, mid_dim), nn.ReLU(),
             nn.Linear(mid_dim, action_dim)
         )
 
@@ -65,29 +56,37 @@ class QNet(nn.Module):
         # return Q(s, a). the estimated state-action value.
         return self.encoder(state)
 
+    def load_and_save_weight(self, path, mode='load'):
+        if mode == 'load':
+            if os.path.exists(path):
+                self.load_state_dict(torch.load(path))
+        else:
+            torch.save(self.state_dict(), path)
+
 
 class DeepQnetwork:
     def __init__(self, obs_dim: int, action_dim: int):
         self.obs_dim = obs_dim
         self.action_dim = action_dim
-        self.learning_tate = 5e-4
-        self.tau = 1e-3  # soft update.
+        self.learning_tate = 1e-4
+        self.tau = 2 ** -8  # soft update.
         self.gamma = 0.99  # discount factor.
         self.batch_size = 1024
-        self.memory_size = 10000
+        self.memory_size = 100000
         self.explore_rate = 0.2  # epsilon greedy rate.
         '''
         for exploring in the env, each time will collect self.target_step * self.batch_size number of samples into buffer,
         for updating neural network, each time will update self.target_step * self.repeat_time times. 
         '''
-        self.target_step = 10
-        self.repeat_time = 100
+        self.target_step = 1024
+        self.repeat_time = 1
         self.reward_scale = 1.
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.buffer = ReplayBuffer(obs_dim, self.batch_size, self.memory_size, self.device)
+        self.buffer = ReplayBuffer(obs_dim, self.memory_size, self.device)
         self.QNet = QNet(obs_dim, action_dim).to(self.device)
-        self.QNet_target = QNet(obs_dim, action_dim).to(self.device) # Q target.
+        self.QNet_target = QNet(obs_dim, action_dim).to(self.device)  # Q target.
         self.optimizer = Adam(self.QNet.parameters(), self.learning_tate)
+        self.loss_func = nn.MSELoss(reduction='mean')
 
     def select_action(self, state: np.ndarray) -> int:
         # using epsilon greedy algorithm to select the action.
@@ -99,68 +98,86 @@ class DeepQnetwork:
             action = dist.argmax(dim=0).cpu().numpy()
         return action
 
-    def explore_env(self, env, all_greedy=False) -> None:
+    def explore_env(self, env, all_greedy=False) -> int:
         # to collect samples into replay buffer.
         state = env.reset()
-        for _ in range(self.target_step * self.batch_size):
+        for _ in range(self.target_step):
             action = np.random.randint(self.action_dim) if all_greedy else self.select_action(state)
             state_, reward, done, _ = env.step(action)
-            self.buffer.store_transition(state, action, reward * self.reward_scale, 0. if done else self.gamma, state_)
+            other = (reward * self.reward_scale, 0.0 if done else self.gamma, action)
+            self.buffer.append(state, other)
             state = env.reset() if done else state_
+        return self.target_step
 
-    def soft_update(self) -> None:
+    @staticmethod
+    def soft_update(eval_net, target_net, tau) -> None:
         # soft update for network. the equation: W_1 * tau + W_2 * (1 - tau)
-        for target_param, local_param in zip(self.QNet_target.parameters(), self.QNet.parameters()):
-            target_param.data.copy_(self.tau * local_param.data + (1.0 - self.tau) * target_param.data)
+        for target_param, local_param in zip(target_net.parameters(), eval_net.parameters()):
+            target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
 
     def update(self) -> None:
         # update the neural network.
         for _ in range(self.target_step * self.repeat_time):
-            state, action, reward, mask, state_ = self.buffer.sample_batch()
+            state, action, reward, mask, state_ = self.buffer.sample_batch(self.batch_size)
             # Q(s_t, a_t) = r_t + \gamma * max Q(s_{t+1}, a)
-            next_q = self.QNet_target(state_).detach().max(dim=1, keepdim=True)[0]
+            next_q = self.QNet_target(state_).detach().max(1)[0]
             q_target = reward + mask * next_q
             q_eval = self.QNet(state).gather(1, action)
-            loss = nn.MSELoss(reduction='mean')(q_eval, q_target)
+            loss = self.loss_func(q_eval, q_target.view(self.batch_size, 1))
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
-            self.soft_update()
+            self.soft_update(self.QNet, self.QNet_target, self.tau)
+
+    def evaluate(self, env):
+        epochs = 50
+        res = np.zeros((epochs,))
+        obs = env.reset()
+        index = 0
+        while index < epochs:
+            obs = torch.as_tensor((obs,), dtype=torch.float32, device=self.device).detach_()
+            dist = self.QNet(obs)[0]
+            action = dist.argmax(dim=0).cpu().numpy()
+            s_, reward, done, _ = env.step(action)
+            res[index] += reward
+            if done:
+                index += 1
+                obs = env.reset()
+            else:
+                obs = s_
+        return res.mean()
+
+
+def render_test():
+    pass
 
 
 def demo_test():
-    def test_agent(env, agent, times=50):
-        res = []
-        for _ in range(times):
-            r = 0.
-            obs = env.reset()
-            while True:
-                action = agent(torch.as_tensor(obs, dtype=torch.float32, device=torch.device('cuda'))).detach_()
-                action = action.argmax(dim=0).cpu().numpy()
-                obs_, reward, done, _ = env.step(action)
-                r += reward
-                if done:
-                    res.append(r)
-                    break
-        return np.array(res).mean()
-
+    import time
     import gym
     from copy import deepcopy
     torch.manual_seed(0)
-    env = gym.make('LunarLander-v2')
+    env_id = 'CartPole-v0'
+    env = gym.make(env_id)
     obs_dim = env.observation_space.shape[0]
     action_dim = env.action_space.n
     agent = DeepQnetwork(obs_dim, action_dim)
     # using random explore to collect samples.
     agent.explore_env(deepcopy(env), all_greedy=True)
-    epochs = 10000
+    total_step = 100000
     eval_env = deepcopy(env)
-    for epoch in range(epochs):
-        agent.explore_env(env)
+    step = 0
+    target_return = 200
+    mr = 0
+    t = time.time()
+    while step < total_step and  mr < target_return-1:
+        step += agent.explore_env(env)
         agent.update()
-        if epoch % 5 == 0:
-            mr = test_agent(eval_env, agent.QNet)
-            print(f'current epoch:{epoch}, reward:{mr}')
+        mr = agent.evaluate(eval_env)
+        print(f'current step:{step}, reward:{mr}')
+    agent.QNet.load_and_save_weight(f'DQN.weight', mode='save')
+    t = time.time() - t
+    print('total cost time:',t,'s')
 
 
 if __name__ == '__main__':
