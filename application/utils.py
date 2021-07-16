c70681ce09d14d5599436f1ead119df9089c6902
HongyingMa <1729037772@sjtu.edu.cn>
2021-07-16 09:49:05 +0800

    add Dueling DQN.

--- application/utils.py
+++ application/utils.py
@@ -11,3 +11,21 @@ def plot_learning_curve(steps, avg_return, std_return, name='plot_learning_curve
     plt.legend()
     plt.savefig(name)
     plt.close()
+
+
+
+def test_on_highway_env():
+    import gym
+    import highway_env
+    from DuelingDQN import DeepDuelingQNetwork
+    env = gym.make('roundabout-v0')
+    env = gym.wrappers.FlattenObservation(env)
+    obs_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
+    agent = DeepDuelingQNetwork(obs_dim, action_dim)
+    agent.DuelingQNet.load_and_save_weight('HighwayDQN.weight', mode='load')
+    agent.evaluate(env, True)
+
+
+if __name__ == '__main__':
+    test_on_highway_env()
